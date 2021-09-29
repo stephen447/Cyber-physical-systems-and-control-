012d9b9f08d72a3572547367838ecb77de3f4049
Keshav Sapkota <sapkotak@tcd.ie>
2021-09-29 10:05:05 +0100

    fixed offest and scaling formulas

--- Drone.py
+++ Drone.py
@@ -7,9 +7,15 @@
 # Write your code here :-)
 from microbit import *  # NEEDS TO BE INCLUDED IN ALL CODE WRITTEN FOR MICROBIT
 import radio  # WORTH CHECKING OUT RADIO CLASS IN BBC MICRO DOCS
+import micropython
 
 radio.on()  # TURNS ON USE OF ANTENNA ON MICROBIT
 radio.config(channel=77)  # A FEW PARAMETERS CAN BE SET BY THE PROGRAMMER
+micropython.kbd_intr(-1)
+
+#initialize UART communication
+uart.init(baudrate=115200, bits=8, parity=None, stop=1, tx=pin1, rx=pin2)
+#uart.init(baudrate=115200, bits=8, parity=None, stop=1, tx=None, rx=None)
 
 # INITIALISE COMMANDS (PARTY)
 pitch = 0
@@ -29,6 +35,7 @@ offset1 = 512
 offset2 = 521  # for roll
 
 
+
 # channel ID, Each need
 roll_id = 0
 pitch_id = 1
@@ -38,38 +45,50 @@ arm_id = 4
 flight_mode_id = 5
 buzzer_id = 6
 
+incoming = ""
+
 # buffer
-# buffer = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+#buffer = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+#buffer = bytearray(16)
+
+#buffer = []
+#buffer.extend(range(0,16,1))
 buffer = bytearray(16)
 
 def load_buffer():
-    # mask_lsb = 0b00111111
-    # mask_msb = 0b11000000
+    # mask_lsb = 0b0011111111
+    # mask_msb = 0b1100000000
     mask_lsb = 255
-    mask_msb = 768
-    
+    mask_msb = 3
+
     # Given
     buffer[0] = 0
     buffer[1] = 0x01
 
     # Roll
-    buffer[2] = (roll_id << 2) | ((mask_msb & scaled_roll) >> 8)
+    buffer[2] = (roll_id << 2) | (((scaled_roll) >> 8) & mask_msb)
     buffer[3] = mask_lsb & scaled_roll
 
     # Pitch
-    buffer[4] = (pitch_id << 2) | ((mask_msb & scaled_pitch) >> 8)
+    buffer[4] = (pitch_id << 2) | (((scaled_pitch) >> 8) & mask_msb)
     buffer[5] = mask_lsb & scaled_pitch
 
     # Throttle
-    buffer[6] = (throttle_id << 2) | ((mask_msb & scaled_throttle) >> 8)
+    buffer[6] = (throttle_id << 2) | (((scaled_throttle) >> 8) & mask_msb)
+    #print(int((throttle_id << 2) | ((mask_msb & scaled_throttle) >> 8)))
     buffer[7] = mask_lsb & scaled_throttle
+    print(buffer[6])
+    print(buffer[7])
+    #print(int(mask_lsb & scaled_throttle))
+    #buffer[6] = 10
+    #buffer[7] = 10
 
     # Yaw
-    buffer[8] = (yaw_id << 2) | ((mask_msb & scaled_yaw) >> 8)
+    buffer[8] = (yaw_id << 2) | (((scaled_yaw) >> 8) & mask_msb)
     buffer[9] = mask_lsb & scaled_yaw
 
     # Arm
-    buffer[10] = (arm_id << 2) | ((mask_msb & scaled_arm) >> 8)
+    buffer[10] = (arm_id << 2) |  (((scaled_arm) >> 8) & mask_msb)
     buffer[11] = mask_lsb & scaled_arm
 
     # Flightmode
@@ -80,27 +99,31 @@ def load_buffer():
     buffer[14] = (buzzer_id << 2) | ((mask_msb & buzzer) >> 8)
     buffer[15] = mask_lsb & buzzer
 
+    #print(buffer)
+
+
 def display_throttle():
-    if throttle > 0:
+    if scaled_throttle > 0:
         display.set_pixel(5, 5, 9)
-    if throttle > 20:
-        display.set_pixel(4, 4, 9)
-    if throttle > 50:
-        display.set_pixel(3, 3, 9)
+    if scaled_throttle > 20:
+        display.set_pixel(5, 4, 9)
+    if scaled_throttle > 50:
+        display.set_pixel(5, 3, 9)
+
 
 # NO NEED TO MAKE FUNCTIONS FOR THIS CONTROLLER
 # JUST USE WHILE LOOP
 while True:
 
-    # INITIALISE COMMAND OUTPUT STRING
-    incoming = ""
-
     incoming = radio.receive()
 
     # need to consider if everything should be inside incoming or can it be outside??
     if incoming:
-        # display.scroll(incoming)
+        #display.scroll(incoming)
+
         parsed_incoming = incoming.split("|", 5)
+        #print(parsed_incoming)
+
         pitch = int(parsed_incoming[0])
         arm = int(parsed_incoming[1])
         roll = int(parsed_incoming[2])
@@ -108,31 +131,34 @@ while True:
         yaw = int(parsed_incoming[4])
         #display.scroll(throttle)
 
-    if arm == 1:
-        display.clear()
-        display.set_pixel(1, 1, 9)
+        if arm == 1:
+            display.clear()
+            display.set_pixel(1, 1, 9)
 
-        # command need to be scaled and offset before going into buffer
-        scaled_pitch = round(scale1 * pitch + offset1)
-        scaled_roll = round(scale1 * roll + offset2)
-        scaled_throttle = round(scale1 * throttle + offset1)
-        scaled_yaw = round(scale2 * yaw + offset1)
-        scaled_arm = round(scale2 * arm + offset1)
-        scaled_throttle = round((throttle * offset1) / 50)  # round to nearest decimal
+            # command need to be scaled and offset before going into buffer
+            scaled_pitch = int(scale1 * pitch + offset1)
+            scaled_roll = int(scale1 * roll + offset2)
+            scaled_throttle = int(scale1 * throttle + offset1)
+            scaled_yaw = int(scale2 * yaw + offset1)
+            #scaled_arm = int(180 * scale2 * arm )
+            scaled_arm = 900
+            scaled_throttle = int((scaled_throttle * offset1) / 1000)  # round to nearest decimal
+            print(scaled_throttle)
 
-        load_buffer()
+            load_buffer()
 
-        # buffer = bytearray(buffer)
-        # initialize UART communication
-        uart.init(baudrate=115200, bits=8, parity=None, stop=1, tx=pin0, rx=pin1)
+            #uart.write(bytes(buffer))
+            uart.write(buffer)
+            #print(uart.write(buffer))
 
-        uart.write(bytes(buffer))
+            #if uart.any():
+            #    display.show(Image.HAPPY)
 
-    # display_throttle()
+            #display_throttle()
 
-    if arm == 0:
-        # pixel (0,0) lights up.
-        display.clear()
-        display.set_pixel(0, 0, 9)
+        elif arm == 0:
+            # pixel (0,0) lights up.
+            display.clear()
+            display.set_pixel(0, 0, 9)
 
     sleep(150)
--- TX_CONTROLLER.py
+++ TX_CONTROLLER.py
@@ -1,9 +1,13 @@
 from microbit import *  # NEEDS TO BE INCLUDED IN ALL CODE WRITTEN FOR MICROBIT
 import radio   # WORTH CHECKING OUT RADIO CLASS IN BBC MICRO DOCS
+import micropython
 
 radio.on()  # TURNS ON USE OF ANTENNA ON MICROBIT
 radio.config(channel = 77)  # A FEW PARAMETERS CAN BE SET BY THE PROGRAMMER
 # channel can be 0-83
+
+#initialize UART communication
+uart.init(baudrate=115200, bits=8, parity=None, stop=1, tx=None, rx=None)
 
 # INITIALISE COMMANDS (PARTY)
 pitch = 0
@@ -15,22 +19,21 @@ yaw = 0
 # NO NEED TO MAKE FUNCTIONS FOR THIS CONTROLLER
 # JUST USE WHILE LOOP
 display.set_pixel(0, 0, 9)
-while True:
 
-    # INITIALISE COMMAND OUTPUT STRING
-    command = ""
+# INITIALISE COMMAND OUTPUT STRING
+command = ""
+
+while True:
 
     # CHECK OUT is_pressed() AND was_pressed() FUNCTIONS
     # FIGURE OUT WHICH FUNCTION IS BETTER SUITED FOR THE
-    # CONTROLLER - "WAS" better as using
-    # "IS" could be caught out by sleep timer
 
     # ARM THE DRONE USING BOTH BUTTONS
     # need is_pressed for arming, because we press a and then b after some time,
     # this will result in arming.
     if button_a.is_pressed() and button_b.is_pressed():
         # if button a and b was pressed - arm / disarm depending on current state
-        throttle = 0
+        throttle = 5
         if arm == 0:
             arm = 1
             # without arm drone cannot do anything, as soon as both buttons are pressed,
@@ -55,12 +58,12 @@ while True:
     # If button b was pressed decrease throttle by 5
     if arm == 1 and button_b.was_pressed():
         throttle = throttle + 5
-        # display.scroll(throttle)
+        display.scroll(throttle)
 
     # USE ACCLEREROMETER CLASS FOR DEALING WITH ROLL, PITCH AND YAW (X, Y AND Z AXES)
-    if accelerometer.was_gesture('shake'):  # Killswitch - using the predefined gestures
-        arm = 0
-        throttle = 0
+    #if accelerometer.was_gesture('shake'):  # Killswitch - using the predefined gestures
+    #    arm = 0
+    #    throttle = 0
 
     # Using accelerometer class to get rotation in x-axis
     roll = accelerometer.get_x()
@@ -73,11 +76,20 @@ while True:
 
     # UPDATE COMMAND STRING TO BE SENT OUT WITH CONCATENATED PARTY COMMANDS
 
+    # For debugging Delete this later on
+    pitch = 0
+    roll = 0
+    yaw = 0
+
+    # do you need to include this at the end of command "\n" ???
     command = str(pitch) + "|" + str(arm) + "|" + str(roll) + "|" + str(throttle) + "|" + str(yaw)
     # display.scroll(str(arm))
     radio.send(command)  # Send command via radio
+    #uart.write(command)
+    print(command)
+    # display.scroll(command)
 
     sleep(100)
-    # Picked 10, as most common in online examples. sleep() IS YOUR FRIEND, FIND GOOD
+    # sleep() IS YOUR FRIEND, FIND GOOD
     # VALUE FOR LENGTH OF SLEEP NEEDED TO FUNCTION WITHOUT COMMANDS GETTING MISSED BY
     # THE DRONE

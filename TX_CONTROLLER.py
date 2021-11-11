c9d5e42e5d90f138513c4be4373ec48987f47267
Keshav Sapkota <sapkotak@tcd.ie>
2021-11-11 17:55:43 +0000

    Fallback, think pid algorithms are correct, play with the parameters

--- TX_CONTROLLER.py
+++ TX_CONTROLLER.py
@@ -48,11 +48,11 @@ roll_pin = pin2
 # Need to find a way of calculating these values
 t2 = 0
 t1 = 0
-dt = 15
+dt = 20
 
 
 # Variables for throt0tle PID control
-throttle_target = 600
+throttle_target = 650
 throttle_current = 0
 throttle_new_error = 0
 throttle_old_error = 0
@@ -61,15 +61,12 @@ throttle_pid_corr = 0
 
 
 # Variables for pitch PID control
-pitch_target = 0
+
 pitch_current = 0
 pitch_new_error = 0
 pitch_old_error = 0
 pitch_error_area = 0
-pitch_kp = 0.1
-pitch_ki = 0.00001
-pitch_kd = 0
-pitch_pid_corr = 0
+pitch_target = 0
 
 
 # Variables for roll PID control
@@ -78,21 +75,17 @@ roll_current = 0
 roll_new_error = 0
 roll_old_error = 0
 roll_error_area = 0
-midpoint = 508
 
 
-roll_pid_corr = 0
-
-roll_calibration = 6 # initially on a flat surface, drone roll is reading -6
-
+#roll_calibration = 6 # initially on a flat surface, drone roll is reading -6
 
 """************************************************************
 
 ************************************************************"""
-throttle_kp = 0.01 # these values seem reasonable
+throttle_kp = 0.008 # these values seem reasonable
 throttle_ki = 0.000001 #0.000001 #0.00001
 # kd not so relevant for throttle
-throttle_kd = 2 #usually controllers use PI system so stick with PI for now
+throttle_kd = 5 #usually controllers use PI system so stick with PI for now
 def throttle_pid_control():
     # Height of the drone can be obatined using geometric properties of encoder
 
@@ -127,12 +120,18 @@ def throttle_pid_control():
     #print("throttle_i_corr", throttle_i_corr)
     #print("throttle_d_corr", throttle_d_corr)
     print((throttle_target,throttle_current,throttle_pid_corr))
+    #print("throttle", throttle_pid_corr)
     return throttle_pid_corr
 
 
 """************************************************************
 
 ************************************************************"""
+pitch_kp = 0.05
+pitch_ki = 0.00001
+pitch_kd = 5
+pitch_target = 0
+pitch_pid_corr = 0
 def pitch_pid_control():
     #print("inside pitch pid")
     global pitch_new_error, pitch_pid_corr, pitch_error_area, pitch_current
@@ -142,9 +141,11 @@ def pitch_pid_control():
     # try changing pitch_target to pitch input parameter,
     # this might improve control with transmitter
 
-    pitch_target = 0
+    pitch_current = mapping(1023-pitch_pin.read_analog(), 0, 1023, -15, 15) + pitch_pid_corr
+    #print("pitch_curr", pitch_current)
+
     #pitch_current = mapping(accelerometer.get_y(),-1023,1023,-90,90)
-    pitch_current=-mapping(int(pitch_pin.read_analog()),0,1023,-90,90)
+    #pitch_current=-mapping(int(pitch_pin.read_analog()),0,1023,-90,90)
 
     pitch_old_error = pitch_new_error
 
@@ -167,38 +168,31 @@ def pitch_pid_control():
     pitch_d_corr = pitch_kd * pitch_error_slope
     # print("pitch_d_corr", pitch_d_corr)
 
-    pitch_pid_corr = int(pitch_pid_corr + pitch_p_corr + pitch_i_corr + pitch_d_corr)
+    pitch_pid_corr = pitch_pid_corr + pitch_p_corr + pitch_i_corr + pitch_d_corr
     #print(pitch_pid_corr)
-    #print((0,0,pitch_pid_corr))
+    #print((pitch_target, pitch_current ,pitch_pid_corr))
     return pitch_pid_corr
 
 
 """************************************************************
 
 ************************************************************"""
+roll_kp = 0.05       # (0.2 - 0.3)
+roll_ki = 0.00001 #0.00001# somewhere around 0.002
+roll_kd = 5 #10 #4 #10
+roll_target = 0 # to make the drone hover our roll target is 512, centre of joystick
+roll_pid_corr=0
 def roll_pid_control():
-    roll_kp = 0.1       # (0.2 - 0.3)
-    roll_ki = 0.0001 # somewhere around 0.002
-    roll_kd = 10
     #print("inside roll pid")
-    global roll_new_error, roll_pid_corr, roll_error_area, roll_current
+    global roll_new_error, roll_pid_corr, roll_error_area, roll_current, roll_p_corr
 
-    # to make the drone hover our roll target is 0
-    # and current roll = drone's accelerometer
-    # try changing roll_target to roll input parameter,
-    # this might improve control with transmitter
-
-    roll_target = midpoint
     #roll_current = mapping(accelerometer.get_x(),-1024,1024,-90,90) + roll_calibration
     #roll_current=-mapping(int(roll_pin.read_analog()),0,1023,-90,90)
 
-    roll_current=int(roll_pin.read_analog())
-    pitch_current = int(1023-pitch_pin.read_analog())
-    #print(roll_current)
-    #print("p", pitch_current)
-
-    #print(int(roll_pin.read_analog()))
+    roll_current= mapping(1023-roll_pin.read_analog(), 0, 1023, -15, 15) + roll_pid_corr
     #print("roll_curr", roll_current)
+    #roll_current = roll_pid_corr
+
     roll_old_error = roll_new_error
 
     # roll_current should match similar mapping
@@ -206,28 +200,22 @@ def roll_pid_control():
 
     # Proportional
     roll_p_corr = roll_kp * roll_new_error
-    #print(roll_p_corr)
-    #print("roll_p_corr", roll_p_corr)
+
     # Integral
-    # following value is too high at the start
     roll_error_area = roll_error_area + (dt * roll_new_error)
-    #print("err_area", roll_error_area)
     roll_i_corr = roll_ki * roll_error_area
-    #print("roll_i_corr", roll_i_corr)
 
     # Differential
     roll_error_change = roll_new_error - roll_old_error
     roll_error_slope = roll_error_change / dt
     roll_d_corr = roll_kd * roll_error_slope
-    #print("roll_d_corr", roll_d_corr)
 
+    roll_pid_corr =  roll_pid_corr + roll_p_corr + roll_i_corr + roll_d_corr
+    #print(roll_pid_corr)
     #print("p_corr",roll_p_corr)
     #print("i_corr",roll_i_corr)
     #print("d_corr", roll_d_corr)
-    roll_pid_corr = int( roll_pid_corr + roll_p_corr + roll_i_corr + roll_d_corr)
-    #print("roll_pid_corr", roll_pid_corr)
-    #print((0,roll_current,roll_pid_corr))
-    #print((roll_current, roll_pid_corr, roll_target))
+    #print((roll_target, roll_current, roll_pid_corr))
     return roll_pid_corr
 
 
@@ -384,38 +372,27 @@ while True:
     #throttle=mapping(throttle, 0,100,0,1023)
     if arm == 1:
 
-        # Roll
-        roll = 0
-        #roll=mapping(accelerometer.get_x(),-1024,1024,-90,90)
-        #roll=-mapping(int(pin0.read_analog()),0,1023,-90,90)
-        if roll>90: roll=90
-        if roll<-90: roll=-90
-        #print("roll ", roll)
-
-
-        # Pitch
-        pitch = 0
-        #pitch=-mapping(accelerometer.get_y(),-1024,1024,-90,90)
-        #pitch=-mapping(int(pin1.read_analog()),0,1023,-90,90)
-        if pitch>90: pitch=90
-        if pitch<-90: pitch=-90
-        #print("pitch ", pitch)
-
-        yaw = 0
-
         # PID CONTROL
         throttle = int(throttle_pid_control())
-        #print((throttle,0,0))
-        #pitch = pitch_pid_control()
-        #roll = roll_pid_control()
+        pitch = int(pitch_pid_control()) #mapping(int(pitch_pid_control()),0,1023, -15,15)
+        # -15 to 15 seems more stable
+        roll = int(roll_pid_control())# mapping(,0,1023, -15,15)
         #print((0,0, roll))
-
 
         if throttle > 1023:
             throttle = 1023
         if throttle < 0:
             throttle = 0
-        command = ""+","+str(pitch)+","+str(arm)+","+str(roll)+","+str(throttle)+","+str(yaw)
+        if roll > 15:
+            roll = 15
+        if roll < -15:
+            roll = -15
+        if pitch > 15:
+            pitch = 15
+        if pitch < -15:
+            pitch = 15
+
+        command = ""+","+str(pitch)+","+str(arm)+","+str(roll)+","+str(throttle)+","+str(0)
         radio.send(command)  # Send command via radio
         #print(command)
 
